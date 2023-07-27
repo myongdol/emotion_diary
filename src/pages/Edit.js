ecf3ba0635eff0080ce39d29e96d3f380059d0df
yong seock <myongdol132@gmail.com>
2023-07-27 19:57:15 +0900

    Page Moving

--- src/pages/Edit.js
+++ src/pages/Edit.js
@@ -1,4 +1,4 @@
-import { useSearchParams } from "react-router-dom";
+import { useNavigate, useSearchParams } from "react-router-dom";
 
 const Edit = () => {
 
@@ -9,10 +9,19 @@ const Edit = () => {
     const mode = searchParams.get('mode');
     console.log("mode :",mode)
 
+    const navigate = useNavigate();
+
     return <div>
         <h1>Edit</h1>
         <p>일기 수정 페이지</p>
-        <button onClick={()=>setSearchParams({who:'myongdol'})}>QS변경하기</button>
+        <button onClick={()=>setSearchParams({who:'myongdol'})}>
+            QS변경하기
+        </button>
+        <button onClick={()=> {
+            navigate("/home")
+        }}>
+            홈으로
+        </button>
     </div>
 }
 
