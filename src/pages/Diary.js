8a5eba6b0f912a88d672c55b16cd9fb7b19f02cf
yong seock <myongdol132@gmail.com>
2023-07-27 19:47:34 +0900

    Path Variable

--- src/pages/Diary.js
+++ src/pages/Diary.js
@@ -1,4 +1,12 @@
+import { useParams } from "react-router-dom";
+
+
+
 const Diary = () => {
+    
+    const {id} = useParams();
+    console.log(id)
+
     return <div>
         <h1>Diary</h1>
         <p>일기 상세 페이지</p>
