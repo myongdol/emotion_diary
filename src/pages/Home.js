7aa7c273e22af811cc01a9465c5c2e71a0734eac
yong seock <myongdol132@gmail.com>
2023-07-29 03:06:26 +0900

    [feat]헤더컴포넌트 월 +- 기능구현

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,29 @@
+import { useState } from "react";
+import Header from '../components/Header';
+import MyButton from '../components/MyButton';
+
 const Home = () => {
+
+    const [curDate, setCurDate] = useState(new Date());
+    console.log(curDate)
+    // getMont는 0부터 시작하기 때문애 +!
+    const headText = `${curDate.getFullYear()}년 ${curDate.getMonth()+1}월` 
+
+    const increaseMonth = () => {
+        setCurDate(new Date(curDate.getFullYear(), curDate.getMonth()+1, curDate.getDate()));
+    }
+
+    const decreaseMonth = () => {
+        setCurDate(new Date(curDate.getFullYear(), curDate.getMonth()-1, curDate.getDate()));
+    }
+
+
     return <div>
-        <h1>Home</h1>
-        <p>홈~</p>
+        <Header 
+        headText={headText}
+        leftChild={<MyButton text={'<-'} onClick={decreaseMonth}/>}
+        rightChild={<MyButton text={'->'} onClick={increaseMonth} />}
+        />
     </div>
 }
 
