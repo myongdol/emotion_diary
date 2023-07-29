e141190e02c6de655b7e6640bf9455121a3de4f2
yong seock <myongdol132@gmail.com>
2023-07-29 23:47:15 +0900

    [feat] 필터 옵션 추가

--- src/components/DiaryList.js
+++ src/components/DiaryList.js
@@ -1,5 +1,25 @@
+import { useState } from "react";
+
+
+const sortOtionList = [
+    {value: "lastest", name:"최신순"},
+    {value: "oldest", name:"오래된 순"},
+]
+
+const ControlMenu = ({value, onChange, optionList}) => {
+    return (
+    <select value={value} onChange={(e) => onChange(e.target.value)}>
+        {optionList.map((item, idx) => <option value={item.value} key={idx}>{item.name}</option>)}
+    </select>)
+}
+
 const DiaryList = ({diaryList}) => {
-    return (<div>
+
+    const [sortType, setSortType] = useState('lastest');
+
+    return (
+    <div>
+        <ControlMenu value={sortType} onChange={setSortType} optionList={sortOtionList}/>
         {diaryList.map((item) => (
             <div key={item.id}>{item.content}</div>
         ))}
