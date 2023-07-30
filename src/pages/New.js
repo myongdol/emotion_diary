b05b8ad99f03cc7d8f9a33b151f3662cf8a9652b
yong seock <myongdol132@gmail.com>
2023-07-30 20:46:52 +0900

    [feat]일기작성 페이지 헤더부분 구현

--- src/pages/New.js
+++ src/pages/New.js
@@ -1,8 +1,20 @@
+import Header from "../components/Header";
+import MyButton from "../components/MyButton";
+import {useNavigate} from 'react-router-dom'
+
+
+
 const New = () => {
-    return <div>
-        <h1>New</h1>
-        <p>일기 추가 작성 페이지</p>
+    const navigate = useNavigate();
+
+    return (
+    <div>
+        <Header
+         headText={"새 일기쓰기"} leftChild={<MyButton text={"<- 뒤로가기"} onClick={()=> navigate(-1)}/>}
+        />
+        
     </div>
+    )
 }
 
 export default New;
