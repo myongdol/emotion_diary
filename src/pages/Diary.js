3c91421bb62d17f5c306805233ce531761308e30
yong seock <myongdol132@gmail.com>
2023-07-31 22:51:01 +0900

    [feat] 일기 상세페이지, 일기 정보 가져오는 로직구현

--- src/pages/Diary.js
+++ src/pages/Diary.js
@@ -1,11 +1,20 @@
+import { useContext, useEffect } from "react";
 import { useParams } from "react-router-dom";
+import { DiaryStateContext } from "../App";
 
 
 
 const Diary = () => {
     
     const {id} = useParams();
-    console.log(id)
+    const diaryList = useContext(DiaryStateContext);
+
+    useEffect(() => {
+        if(diaryList.length >= 1) { 
+            const targetDiary = diaryList.find((item) => parseInt(item.id) === parseInt(id));
+            console.log(targetDiary)
+        } 
+    }, [id, diaryList])
 
     return <div>
         <h1>Diary</h1>
