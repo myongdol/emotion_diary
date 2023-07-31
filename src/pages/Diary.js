356bc8ef372d3aedfe35ad12cae332d890345e98
yong seock <myongdol132@gmail.com>
2023-07-31 23:40:45 +0900

    [desing] diary컴포넌트 스타일링

--- src/pages/Diary.js
+++ src/pages/Diary.js
@@ -49,7 +49,7 @@ const Diary = () => {
                 <article>
                     <section>
                         <h4>오늘의 감정</h4>
-                        <div className="diary_img_wrapper">
+                        <div className={["diary_img_wrapper", `diary_img_wrapper_${data.emotion}`].join(" ")}>
                             <img src={curEmotionData.emotion_img} /> 
                             <div className="emotion_descript">
                                 {curEmotionData.emotion_descript}
